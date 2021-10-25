b981012275b8f6bc188abe0bff886c389a5f83a6
Augusto da Silva <augusto.da.silva@live.com>
2021-10-25 01:57:38 -0300

    rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "pessoas#index"
   resources :pessoas
   resources :generos
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
